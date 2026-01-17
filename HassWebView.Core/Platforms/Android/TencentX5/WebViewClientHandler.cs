a88da7b9bc4a6cce7a4007262a26c0601b89dd1f
shaonianzhentan <ilovevar@gmail.com>
2026-01-17 21:33:03 +0800

    update

--- HassWebView.Core/Platforms/Android/TencentX5/WebViewClientHandler.cs
+++ HassWebView.Core/Platforms/Android/TencentX5/WebViewClientHandler.cs
@@ -2,6 +2,7 @@ using Android.Graphics;
 using Com.Tencent.Smtt.Export.External.Interfaces;
 using Com.Tencent.Smtt.Sdk;
 using HassWebView.Core.Events;
+using Java.Net;
 
 namespace HassWebView.Core.Platforms.Android;
 
@@ -17,21 +18,6 @@ public class WebViewClientHandler : WebViewClient
         _webView = webView;
     }
 
-    public override bool ShouldOverrideUrlLoading(WebView view, IWebResourceRequest request)
-    {
-        var url = request.Url.ToString();
-        var args = new WebNavigatingEventArgs(WebNavigationEvent.NewPage, new UrlWebViewSource{ Url = url }, url);
-        _webView.SendNavigating(args);
-
-        if (args.Cancel)
-        {
-            return true;
-        }
-
-        view.LoadUrl(url);
-        return true;
-    }
-
     public override WebResourceResponse ShouldInterceptRequest(WebView view, IWebResourceRequest request)
     {
         var url = request.Url.ToString();
@@ -43,7 +29,25 @@ public class WebViewClientHandler : WebViewClient
         return base.ShouldInterceptRequest(view, request);
     }
 
-    public override void OnPageFinished(global::Com.Tencent.Smtt.Sdk.WebView view, string url)
+    public override void OnPageStarted(WebView view, string url, Bitmap p2)
+    {
+        var args = new WebNavigatingEventArgs(
+        WebNavigationEvent.NewPage,
+        new UrlWebViewSource { Url = url },
+        url);
+
+        _webView.SendNavigating(args);
+
+        if (args.Cancel)
+        {
+            view.StopLoading();
+            return;
+        }
+
+        base.OnPageStarted(view, url, p2);
+    }
+
+    public override void OnPageFinished(WebView view, string url)
     {
         base.OnPageFinished(view, url);
         _webView.SendNavigated(new WebNavigatedEventArgs(WebNavigationEvent.NewPage, new UrlWebViewSource { Url = url }, url, WebNavigationResult.Success));
---
+++ HassWebView.Core/Services/TencentX5Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HassWebView.Core.Services
+{
+    public class TencentX5Service
+    {
+        //#if ANDROID
+        //        var apkPath = "/sdcard/Android/data/com.jiluxinqing.tv/files/tbs.apk";
+        //        Debug.WriteLine(apkPath);
+        //        if (File.Exists(apkPath))
+        //        {
+        //            Com.Tencent.Smtt.Sdk.QbSdk.InstallLocalTbsCore(Platform.CurrentActivity, 0, apkPath);
+        //        }
+        //#endif
+    }
+}
