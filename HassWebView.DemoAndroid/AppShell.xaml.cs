bb67607bef82b0a7323300dacff034f9d7ff422d
shaonianzhentan <ilovevar@gmail.com>
2026-03-14 22:09:40 +0800

    update

---
+++ HassWebView.DemoAndroid/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HassWebView.DemoAndroid
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
