7842867c926f5a4d20d69be73df7300857687fb8
zhulu <635147515@qq.com>
2026-06-04 11:29:52 +0800

    更新设置页面

--- HassWebView.DemoComponent/Views/SettingsPage.xaml.cs
+++ HassWebView.DemoComponent/Views/SettingsPage.xaml.cs
@@ -6,20 +6,40 @@ namespace HassWebView.DemoComponent.Views;
 
 public partial class SettingsPage : ContentPage
 {
+    private string _currentThemeText;
+    public string CurrentThemeText
+    {
+        get => _currentThemeText;
+        set
+        {
+            _currentThemeText = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private string _currentSizeText;
+    public string CurrentSizeText
+    {
+        get => _currentSizeText;
+        set
+        {
+            _currentSizeText = value;
+            OnPropertyChanged();
+        }
+    }
+
     public SettingsPage()
     {
         InitializeComponent();
+        BindingContext = this;
         UpdateCurrentSettings();
         
-        // 订阅主题变化事件
         ThemeManager.ThemeChanged += (s, e) => UpdateCurrentSettings();
     }
 
     private void UpdateCurrentSettings()
     {
-        // 更新当前主题显示
-        var currentTheme = ThemeManager.CurrentTheme;
-        CurrentThemeLabel.Text = currentTheme switch
+        CurrentThemeText = ThemeManager.CurrentTheme switch
         {
             ThemeMode.Dark => "暗色模式",
             ThemeMode.Light => "亮色模式",
@@ -27,13 +47,23 @@ public partial class SettingsPage : ContentPage
             _ => "未知"
         };
 
-        // 更新当前尺寸显示（简单实现）
-        CurrentSizeLabel.Text = SizeManager.CurrentSize.ToString();
+        CurrentSizeText = SizeManager.CurrentSize switch
+        {
+            ComponentSize.Phone => "手机",
+            ComponentSize.Tablet => "平板",
+            ComponentSize.TV => "电视",
+            _ => "未知"
+        };
     }
 
     protected override void OnAppearing()
     {
         base.OnAppearing();
         UpdateCurrentSettings();
+    }
+
+    private void OnAboutClicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new AboutPage());
     }
 }
