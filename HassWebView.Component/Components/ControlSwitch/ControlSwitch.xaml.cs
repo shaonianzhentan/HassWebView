0743632c07f75b8aec64f08fbb0ec01f05327a39
zhulu <635147515@qq.com>
2026-06-08 15:56:53 +0800

    优化开关组件显示

--- HassWebView.Component/Components/ControlSwitch/ControlSwitch.xaml.cs
+++ HassWebView.Component/Components/ControlSwitch/ControlSwitch.xaml.cs
@@ -1,6 +1,7 @@
 namespace HassWebView.Component.Components;
 
 using HassWebView.Component.Components.Base;
+using HassWebView.Component.Models;
 
 public partial class ControlSwitch : AdaptiveComponent
 {
@@ -11,10 +12,40 @@ public partial class ControlSwitch : AdaptiveComponent
     public static readonly BindableProperty DisabledProperty =
         BindableProperty.Create(nameof(Disabled), typeof(bool), typeof(ControlSwitch), false);
 
+    private bool _isInitialized;
+
     public ControlSwitch()
     {
         InitializeComponent();
-        UpdateThumbPosition(false);
+        Loaded += OnLoaded;
+        SizeManager.SizeChanged += OnGlobalSizeChanged;
+    }
+
+    private void OnLoaded(object? sender, EventArgs e)
+    {
+        Application.Current?.Dispatcher.DispatchDelayed(TimeSpan.FromMilliseconds(100), () =>
+        {
+            UpdateThumbPosition(false);
+            _isInitialized = true;
+        });
+        Loaded -= OnLoaded;
+    }
+
+    private void OnGlobalSizeChanged(object? sender, EventArgs e)
+    {
+        if (_isInitialized)
+        {
+            UpdateThumbPosition(false);
+        }
+    }
+
+    protected override void OnPropertyChanged(string? propertyName = null)
+    {
+        base.OnPropertyChanged(propertyName);
+        if (propertyName == nameof(EffectiveSize) && _isInitialized)
+        {
+            UpdateThumbPosition(false);
+        }
     }
 
     public bool IsToggled
@@ -44,7 +75,48 @@ public partial class ControlSwitch : AdaptiveComponent
     {
         try
         {
-            double maxTranslation = SwitchGrid.WidthRequest - Thumb.WidthRequest - 4;
+            double gridWidth, gridHeight, thumbWidth, thumbHeight, margin, cornerRadius;
+            
+            switch (EffectiveSize)
+            {
+                case ComponentSize.Tablet:
+                    gridWidth = 76;
+                    gridHeight = 46;
+                    thumbWidth = 40;
+                    thumbHeight = 40;
+                    margin = 6; // 3 * 2
+                    cornerRadius = 23;
+                    break;
+                case ComponentSize.TV:
+                    gridWidth = 102;
+                    gridHeight = 62;
+                    thumbWidth = 54;
+                    thumbHeight = 54;
+                    margin = 8; // 4 * 2
+                    cornerRadius = 31;
+                    break;
+                default: // Phone
+                    gridWidth = 51;
+                    gridHeight = 31;
+                    thumbWidth = 27;
+                    thumbHeight = 27;
+                    margin = 4; // 2 * 2
+                    cornerRadius = 15;
+                    break;
+            }
+            
+            SwitchBorder.WidthRequest = gridWidth;
+            SwitchBorder.HeightRequest = gridHeight;
+            SwitchGrid.WidthRequest = gridWidth;
+            SwitchGrid.HeightRequest = gridHeight;
+            Thumb.WidthRequest = thumbWidth;
+            Thumb.HeightRequest = thumbHeight;
+            Thumb.Margin = new Thickness(margin / 2);
+            BorderStrokeShape.CornerRadius = cornerRadius;
+            
+            double actualGridWidth = SwitchGrid.Width > 0 ? SwitchGrid.Width : gridWidth;
+            double actualThumbWidth = Thumb.Width > 0 ? Thumb.Width : thumbWidth;
+            double maxTranslation = actualGridWidth - actualThumbWidth - margin;
             double targetX = IsToggled ? maxTranslation : 0;
 
             if (animated)
